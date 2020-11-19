8f80d94d7688fd1ed07792e5115c26530ebc2e9e
SANIYA THAHSEEN M <ayinasmadala@gmail.com>
2020-11-19 11:58:21 +0530

    cm

--- app/src/main/java/com/example/face_recog/MainActivity.java
+++ app/src/main/java/com/example/face_recog/MainActivity.java
@@ -6,32 +6,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
 import android.Manifest;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
-public class MainActivity extends AppCompatActivity {
-    EditText e1,e2;
+
+
+public  class MainActivity extends AppCompatActivity  {
+    EditText etUsername,etPassword;
     Button login;
     public static final int CAMERA_PERM_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
+    Bitmap image;
+
+    private View mProgressView;
+    private View mLoginFormView;
+    private static final String AUTH_TOKEN_URL = "http://" +"127.0.0.1:8000/api-token-auth/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        e1=findViewById(R.id.editTextTextPersonName);
-        e2=findViewById(R.id.editTextTextPassword);
+        etUsername=findViewById(R.id.editTextTextPersonName);
+        etPassword=findViewById(R.id.editTextTextPassword);
         login=findViewById(R.id.button);
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
 
@@ -74,13 +104,16 @@ public class MainActivity extends AppCompatActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST_CODE) {
-            Bitmap image = (Bitmap) data.getExtras().get("data");
+             image = (Bitmap) data.getExtras().get("data");
+
+
+
             Intent i = new Intent(getApplicationContext(), DispImageActivity.class);
             i.putExtra("image", image);
             startActivity(i);
 
-            Toast.makeText(this,"camera permission",Toast.LENGTH_SHORT).show();
-
         }
     }
+
+
 }
